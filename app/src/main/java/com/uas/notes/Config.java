434576349a2910787a1d025dbbe97ed3ebe38479
Achmad Juliarman <acmadjuliarman@gmail.com>
2023-08-10 22:08:13 +0700

    Add Notification

--- app/src/main/java/com/uas/notes/Config.java
+++ app/src/main/java/com/uas/notes/Config.java
@@ -8,7 +8,7 @@ package com.uas.notes;
 public class Config {
 
     public static String DB_URL = "https://uas-10120061-if2-default-rtdb.asia-southeast1.firebasedatabase.app/";
-    public static String FCM_SERVER_KEY = "AAAAlTKf4Do:APA91bF1Fy_urLW8P9qk-QKbpQdK7FGJp9v09Uiy1YW409cjZflHiecfOtfLyhBT4PdjuwtYYpm9nlznlPEuEdm0Y2zwBxt8aWANcq0Dw1Rde4fQNZDrzsrYyBgxIsG9pvmjDD-gdc4B";
+    public static String FCM_SERVER_KEY = "AAAATStni9I:APA91bFVhr9swXdhE7QhGTGARiSonL1T9pqyHz1hHMA28dYpBg21LEW0zZGD7VJfAFmmR9uhGJhuneRBK1SZsJ9WEX_7Y0QS_gtUVoIWGsEOEmVu4NK94vBLP6r2Mj3_H4Wavx3XMhTs";
 
     public static String getDB_URL() {
         return DB_URL;
